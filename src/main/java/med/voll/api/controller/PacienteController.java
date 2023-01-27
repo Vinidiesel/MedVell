8372a4e52ee7cfd5b70a2d12c6c28a433d6640f9
Vinicius Inhesta Dos Santos <vinicius.inhesta@hotmail.com>
2023-01-27 17:08:27 -0300

    Listagem de pacientes
    Faz a listagem de pacientes utilizando DTO seguindo a seguinte regra:
    
    A listagem deve ser ordenada pelo nome do paciente, de maneira crescente, bem como ser paginada, trazendo 10 registros por página.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,13 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPacientes;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -24,5 +25,11 @@ public class PacienteController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         pacienteRepository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPacientes> listar(@PageableDefault(size = 10,
+            sort = {"nome"})Pageable pageable){
+        return pacienteRepository.findAll(pageable).map(DadosListagemPacientes::new);
     }
 }
